1c97c281fdcd610b0510fbd09a30200d3b7772b2
Jeremy Eller <jeremy@campusesp.com>
2018-07-26 09:19:41 -0400

    Added a whole bunch of stuff. Tic tac toe and whatnot.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tic_tac_toe/play'
+  get 'tic_tac_toe/game_over'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
